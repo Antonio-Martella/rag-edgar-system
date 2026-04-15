1a402f83fc7302337e201d43595652280c3a7cfa
Antonio-Martella <antony_8@hotmail.it>
2026-04-15 21:30:59 +0000

    feat: executable for downloading and setting up Embedding, Reranking and LLM models

--- scripts/run_setupmodels.py
+++ scripts/run_setupmodels.py
@@ -1,11 +1,10 @@
 import sys
 from pathlib import Path
 
-# 
+# Ensure the project root is in the system path for imports
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 sys.path.append(str(PROJECT_ROOT))
 
-# 
 from src.embedding import setup_embedding_model
 from src.retrieval import setup_reranker_model
 from src.llm import setup_llm
