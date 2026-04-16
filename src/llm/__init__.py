fb43b1e4da30619f6b803b641629e9a95106562b
Antonio-Martella <antony_8@hotmail.it>
2026-04-16 17:29:48 +0000

    feat: add __init__.py to ingestion module
    Let's introduce the basic components: functions and classes for querying llm (setup_llm, quantization_config, LLMGenerator, build_rag_messages).

--- src/llm/__init__.py
+++ src/llm/__init__.py
@@ -1,8 +1,10 @@
 from .model import setup_llm, get_quantization_config
 from .generator import LLMGenerator
+from .prompt import build_rag_messages
 
 __all__ = [
     "setup_llm",
     "get_quantization_config",
-    "LLMGenerator"
+    "LLMGenerator",
+    "build_rag_messages"
 ]
