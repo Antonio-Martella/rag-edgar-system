6591a4dcc20a8b2fdf915a231ef809adf3feeef9
Antonio-Martella <antony_8@hotmail.it>
2026-04-16 12:53:36 +0000

    feat: Updating the routes and models used

--- src/utils/config.py
+++ src/utils/config.py
@@ -4,8 +4,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Load the Hugging Face token from the environment variables
+HF_TOKEN = os.getenv("HF_TOKEN")
+
 # Root of the project
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
+
+# ------------------------------------
+# ------- MODELS CONFIGURATION -------
+# ------------------------------------
+# Let's define the ID of the embedding model, the reranker and the LLM we want to use.
+EMBEDDING_MODEL_ID = "nomic-ai/nomic-embed-text-v1.5"
+RERANKER_MODEL_ID = "BAAI/bge-reranker-v2-m3"
+LLM_MODEL_ID = "mistralai/Mistral-7B-Instruct-v0.2"
+# ------------------------------------
+
+# Local Models Folder
+MODELS_DIR = PROJECT_ROOT / "models"
+
+# Dynamic paths for locally saved models
+LOCAL_EMBEDDING_PATH = MODELS_DIR / EMBEDDING_MODEL_ID
+LOCAL_RERANKER_PATH = MODELS_DIR / RERANKER_MODEL_ID
+LOCAL_LLM_PATH = MODELS_DIR / LLM_MODEL_ID
 
 # Data Folders
 DATA_DIR = PROJECT_ROOT / "data"
@@ -13,33 +33,14 @@ RAW_DATA_DIR = DATA_DIR / "raw"
 CHUNKS_DIR = DATA_DIR / "chunks"
 EMBEDDINGS_DIR = DATA_DIR / "embeddings" 
 
-# Local Models Folder
-MODELS_DIR = PROJECT_ROOT / "models"
-
 # Create all the necessary folders
 for path in [RAW_DATA_DIR, CHUNKS_DIR, EMBEDDINGS_DIR, MODELS_DIR]:
     path.mkdir(parents=True, exist_ok=True)
 
-# ------------------------------------
-# ------- MODELS CONFIGURATION -------
-# ------------------------------------
-# Definiamo l'ID del modello embedding, del reranker e del LLM che vogliamo usare.
-EMBEDDING_MODEL_ID = "nomic-ai/nomic-embed-text-v1.5"
-RERANKER_MODEL_ID = "BAAI/bge-reranker-v2-m3"
-LLM_MODEL_ID = "mistralai/Mistral-7B-Instruct-v0.2"
-
-# Carichiamo il token di Hugging Face dalle variabili d'ambiente
-HF_TOKEN = os.getenv("HF_TOKEN")
-
-# Dynamic paths for locally saved models
-LOCAL_EMBEDDING_PATH = MODELS_DIR / EMBEDDING_MODEL_ID
-LOCAL_RERANKER_PATH = MODELS_DIR / RERANKER_MODEL_ID
-LOCAL_LLM_PATH = MODELS_DIR / LLM_MODEL_ID
-
 # Helper function to get file paths for a given ticker and report type
-def get_paths(ticker, report_type="10-K", date=None):
+def get_paths(ticker: str, report_type: str, date: str) -> dict:
     """
-    Helper per ottenere i percorsi dei file dati per un ticker specifico.
+    Helper to get the paths to data files for a specific ticker.
     """
     t = ticker.lower()
     return {
