1e541e21324692c458923937781fdda17f645141
Antonio-Martella <antony_8@hotmail.it>
2026-04-16 17:46:23 +0000

    feat: add __init__.py to retrieval module

--- src/retrieval/__init__.py
+++ src/retrieval/__init__.py
@@ -1,6 +1,8 @@
 from .reranker import RAGReranker, setup_reranker_model
+from .retriever import Retriever
 
 __all__ = [
     "setup_reranker_model",
-    "RAGReranker"
+    "RAGReranker",
+    "Retriever"
 ]
